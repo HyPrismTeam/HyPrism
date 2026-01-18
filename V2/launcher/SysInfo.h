8c90041e8761fa61537a4d3af6da0e7b598db278
yyyumeniku <gabrielhernandez7443@gmail.com>
2026-01-18 17:58:07 -0500

    Delete V2 branch and unused launcher directory

--- V2/launcher/SysInfo.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <QString>
-
-namespace SysInfo {
-QString currentSystem();
-QString useQTForArch();
-QString getSupportedJavaArchitecture();
-int suitableMaxMem();
-}  // namespace SysInfo
