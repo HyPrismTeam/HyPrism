296390d443a878868272922e7e3f86ca9d614f65
Denis <msoxrap857@gmail.com>
2026-02-03 12:29:15 +0200

    Delete scripts/appimage_installer/test.py

--- scripts/appimage_installer/test.py
+++
@@ -1,6 +0,0 @@
-from PyInstaller.utils.hooks import collect_all, collect_submodules
-
-datas, binaries, hiddenimports = collect_all('requests')
-
-# Также добавляем все подмодули requests
-hiddenimports += collect_submodules('requests')
